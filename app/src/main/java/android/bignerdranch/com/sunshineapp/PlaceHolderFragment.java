0ddcf4cfb958fd3d7a50dfd3ca2426dba88563e4
slad08 <slad_den@mail.ru>
2016-02-16 00:18:32 +0300

    Lesson 2.HttpConneection add

--- app/src/main/java/android/bignerdranch/com/sunshineapp/PlaceHolderFragment.java
+++ app/src/main/java/android/bignerdranch/com/sunshineapp/PlaceHolderFragment.java
@@ -11,6 +11,13 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class PlaceHolderFragment extends Fragment {
 
     public final String TAG="PlaceHolderFragment";
@@ -50,7 +57,60 @@ public class PlaceHolderFragment extends Fragment {
 
         Log.d(TAG,"PlaceHolderFragment работа фрагмеента");
 
-        
+        HttpURLConnection urlConnection = null;
+        BufferedReader readr = null;
+
+        //  Будет храниться JSON ответ как строка
+        String forecastJsonStr = null;
+
+        try {
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&units=metric&cnt=7&APPID=fc98f6f48ac0446ca20c391650c87479");
+       //Создаем запрос к OpenWeatherMap, и открываем соединение
+            urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            //Читаем входной потов в String
+            InputStream inpuStream = urlConnection.getInputStream();
+            StringBuffer buff = new StringBuffer();
+
+            if (inpuStream == null){
+                //Ничего не делаем
+                forecastJsonStr = null;
+            }
+            readr = new BufferedReader(new InputStreamReader(inpuStream));
+
+            String linee;
+
+            while ((linee = readr.readLine())!=null){
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buff.append(linee + "\n");
+            }
+            if (buff.length()==0){
+                //Поток был пустой.Нет точек для парсинга
+                forecastJsonStr = null;
+            }
+            forecastJsonStr = buff.toString();
+
+        }catch (IOException ee){
+            Log.e("PlaceHolderFragment","Error",ee);
+            //Если когд не получит успешно данные о погоде, здесь не указываются попытки распарсить это
+            forecastJsonStr = null;
+        } finally {
+            if (urlConnection !=null){
+                urlConnection.disconnect();
+            }
+            if (readr !=null){
+                try {
+                    readr.close();
+                }catch (final IOException ee){
+                    Log.e("PlaceHolderFragment","Error closing stream", ee);
+                }
+            }
+        }
+
         return rootView;
     }
 }
